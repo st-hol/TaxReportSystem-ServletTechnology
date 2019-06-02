b990d8a9e5e3569e0a5b9beea35aab4fa9c63a3d
Stas <stanislav.holovachuk@gmail.com>
2019-06-02 18:15:41 +0300

    regexes changed

--- src/main/java/ua/training/model/validator/NumberValidator.java
+++ src/main/java/ua/training/model/validator/NumberValidator.java
@@ -5,7 +5,6 @@ import java.util.regex.Pattern;
 public class NumberValidator {
 
     public static boolean validateNumber(String value) {
-        //System.out.println(value != null && Pattern.matches(Regexes.NUMBER_REGEXP.getREGEXP(), value));
         return value != null && Pattern.matches(Regexes.NUMBER_REGEXP.getREGEXP(), value);
     }
 
--- src/main/java/ua/training/model/validator/UserValidator.java
+++ src/main/java/ua/training/model/validator/UserValidator.java
@@ -7,12 +7,10 @@ import java.util.regex.Pattern;
 public class UserValidator {
 
     public static boolean validateEmail(String emailValue) {
-        //System.out.println(emailValue != null && Pattern.matches(Regexes.EMAIL_REGEXP.getREGEXP(), emailValue));
         return emailValue != null && Pattern.matches(Regexes.EMAIL_REGEXP.getREGEXP(), emailValue);
     }
 
     public static boolean validatePassword(String passwordValue) {
-        //System.out.println(passwordValue != null && Pattern.matches(Regexes.PASSWORD_REGEXP.getREGEXP(), passwordValue));
         return passwordValue != null && Pattern.matches(Regexes.PASSWORD_REGEXP.getREGEXP(), passwordValue);
     }
 
